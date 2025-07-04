ed7a007b63d3f9c08e7ad5c3ca33eb81bfb94a7e
Tomas Medel <tfmedel@gmail.com>
2025-07-04 11:26:53 -0300

    crear clase Empleado

---
+++ rh/src/main/java/tda/rh/Entidades/Empleado.java
@@ -0,0 +1,25 @@
+package tda.rh.Entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idEmpleado;
+
+    String nombre;
+    String departamento;
+    double sueldo;
+}
