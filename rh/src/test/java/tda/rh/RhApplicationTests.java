784f7025c3b00c2657f7f61833b1db1677a9e115
Tomas Medel <tfmedel@gmail.com>
2025-07-04 11:05:28 -0300

    inicializacion del proyecto

---
+++ rh/src/test/java/tda/rh/RhApplicationTests.java
@@ -0,0 +1,13 @@
+package tda.rh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
